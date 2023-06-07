8b91288422dab18e9e07154df0754e1b3f5eb881
JonRauzy <jonathan.rauzy@gmail.com>
2023-06-07 15:42:44 +0200

    example model

--- model/model.php
+++ model/model.php
@@ -6,6 +6,12 @@ use model\abstractClass\MappingAbstract;
 
 class model extends MappingAbstract
 {
+    
+    public function __construct(array $tab)
+    {
+        parent::__construct($tab);
+    }
+
     public function __toString(): string
     {
         return "class model.php enfants de MappingAbstract.php";
