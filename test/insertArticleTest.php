b1e31d01024251756447f8a1c04d46c95d62b5c8
JonRauzy <jonathan.rauzy@gmail.com>
2023-06-20 15:00:57 +0200

    test 0.2

--- test/insertArticleTest.php
+++ test/insertArticleTest.php
@@ -1,7 +1,14 @@
 <?php
 
+use model\managerClass\ManagerArticle;
 use model\managerClass\ManagerPicture;
-use model\mappingClass\ManagergArticle;
 use model\mappingClass\MappingPicture;
 use model\mappingClass\MappingArticle;
 
+$mapPic = new MappingPicture([]);
+$mapArt = new MappingArticle([]);
+
+$manaPic = new ManagerPicture($db);
+$manaArt = new ManagerArticle($db);
+
+var_dump($mapArt, $mapPic, $manaArt, $manaPic);
