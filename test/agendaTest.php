77568155a7a9d5a64e9f325bb30ca3a6e4703573
JonRauzy <jonathan.rauzy@gmail.com>
2023-06-28 09:56:18 +0200

    test agenda 1

--- test/agendaTest.php
+++ test/agendaTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use model\mappingClass\MappingAgenda;
+use model\managerClass\ManagerAgenda;
 
 
 // require de la config:
@@ -16,18 +17,29 @@ spl_autoload_register(function ($class) {
     require '../' . $class . '.php';
 });
 
+
+try {
+    
+    $db = new PDO(DB_TYPE.':host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME.';charset='.DB_CHARSET,DB_LOGIN,DB_PWD);
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    
+}catch(Exception $e){
+    
+    //die($e->getMessage());
+    echo $e->getMessage();
+    echo "<br>";
+}
+
+// test des mapping :
 try{
 $test1 = new MappingAgenda([
-    "mwIdAgenda" => 1,
-    "mwDateAgenda" => "test",
+    "mwDateAgenda" => "2001-01-01",
     "mwContentAgenda" => "test",
     "mwTitleAgenda" => "test",
-    "mwPictureMwIdPicture" => 1
 ]);
 }catch(Exception $e ){
     $e->getMessage();
 }
-
 
 
 try{
@@ -42,3 +54,19 @@ echo "<pre>";
 var_dump($test1,$test2);
 echo "</pre>";
 
+// test des manager :
+$managerAgenda = new ManagerAgenda($db);
+// var_dump($managerAgenda);
+$getAll = $managerAgenda -> getAll();
+
+var_dump($getAll);
+
+foreach($getAll as $event){
+    echo $event -> getMwIdAgenda();
+    echo $event -> getMwContentAgenda();  
+    // echo "date : " . $event -> getMwDateAgenda() . "<br>";  
+
+}
+
+$insertA = $managerAgenda -> insertAgenda($test1);
+var_dump($insertA);
