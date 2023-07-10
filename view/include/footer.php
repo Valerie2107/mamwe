e0e7408554b0516fc71748318d9c3355108fa372
JonRauzy <jonathan.rauzy@gmail.com>
2023-07-10 16:09:58 +0200

    rectif design

--- view/include/footer.php
+++ view/include/footer.php
@@ -1,8 +1,9 @@
     <!-- pour se connecter, on fera un truc discret dès que ça marche -->
 
     <!-- si on est connecté -->
+
     <?php if(empty($_SESSION)) : ?>
-        <button><a href="?p=connect">CONNECT</a></button>
+        <button class="btn"><a href="?p=connect">CONNECT</a></button>
     <?php endif; ?>
 
     
