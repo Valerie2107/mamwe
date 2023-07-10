84700e7f7eb9a42ece1413758f1b8e5df37a5b31
valerie2107 <kinkamyl@hotmail.com>
2023-07-10 16:19:15 +0200

    formulaire livre d'or

--- view/publicView/livreDorView.php
+++ view/publicView/livreDorView.php
@@ -11,14 +11,15 @@ include_once "../view/include/header.php";
 <!-- HTML -->
 <h1><?= $title ?></h1>
 
-
-
-
-
-
-
-
-
+<section class="container">
+    <form name='livreDor' action='' method="POST">
+        <div class="champs"><label for="lastname">Nom</label><input type="text" name="lastname" placeholder="Indiquez votre nom" required></div>
+        <div class="champs"><label for="firstname">Prénom</label><input type="text" name="firstname" placeholder="Indiquez votre prénom" required></div>
+        <div class="champs"><label for="usermail">E-mail</label><input type='email' name="usermail" placeholder="Indiquez votre e-mail" required></div>
+        <div class="champs"><label for="avis">Votre avis</label><textarea maxlength="600" name="message" placeholder="Indiquez votre avis" required></textarea></div>
+        <div class="envoi"><input id="bouton" type="submit" value="Envoyer"></div>        
+    </form>
+</section>
 
 
 
