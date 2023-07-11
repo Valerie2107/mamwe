4e9849c8c822321532c0d8fd44e23f8b81caa44e
JonRauzy <jonathan.rauzy@gmail.com>
2023-07-11 15:58:25 +0200

    editeur de texte

--- view/privateView/agendaCrud.php
+++ view/privateView/agendaCrud.php
@@ -21,7 +21,8 @@ include_once "../view/include/header.php";
 <!-- Formulaire primitif pour tester le Controller, démerdez vous maintenant : -->
 <form action="" method="POST">
     <input type="text" name="titleAgenda" placeholder="titre" ><br>
-    <input type="text" name="contentAgenda" placeholder="contenu"><br>
+    <!-- y'a le #mytextarea pour relier à l'éditeur de text -->
+    <textarea name="contentAgenda" id="mytextarea" ></textarea>
     <input type="text" name="dateAgenda" placeholder="date"><br>
     <input type="text" name="titlePic" placeholder="titre photo"><br>
     <input type="text" name="urlPic" placeholder="url photo"><br>
