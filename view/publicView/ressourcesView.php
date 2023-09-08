af4ea4a0176f419203bf6497ab8c6bb44c691b4e
valerie2107 <kinkamyl@hotmail.com>
2023-09-08 10:17:36 +0200

    on continue la mise en page des ressources

--- view/publicView/ressourcesView.php
+++ view/publicView/ressourcesView.php
@@ -10,70 +10,70 @@ include_once "../view/include/header.php";
 
 <!-- HTML -->
 <main>
-<h1><?= $title ?></h1>
+    <h1><?= $title ?></h1>
     <div>
         <p>Vous retrouverez ici une mine d'informations sur tous les thèmes qui touchent à la naissance de la préconception à la puériculture en passant par la parentalité.</p>
     </div>
     <div class="empty"></div>
-<div>
     <div>
-<?php
-// on boucle sur les catégories :
+        <div>
+            <?php
+            // on boucle sur les catégories :
 
-foreach($getAllCateg as $categ){
+                foreach($getAllCateg as $categ){
 
-    // on affiche le titre de la catég :
-    echo "<h2 class=''> categ : " . $categ -> getMwTitleCategory() . "<br><br>"; 
-    // on récupère son ID :
-    $categId = $categ->getMwIdCategory();
-?>
-</div>
-<div>
-    <?php
-    // on boucle sur la sous categ:
-    
-    foreach($getAllSub as $sub){
-        // on recupère l'ID:
-        $subId = $sub -> getMwIdSubCategory();
-        
-        // On recupère toutes les ressources avec les ID des categ et sous categ en même temps :
-        $getAllByAll = $ressourceManager -> getAllbyAll($categId, $subId);
+                    // on affiche le titre de la catég :
+                    echo "<h2 class=''> categ : " . $categ -> getMwTitleCategory() . "<br><br>"; 
+                    // on récupère son ID :
+                    $categId = $categ->getMwIdCategory();
+            ?>
+        </div>
+            <div>
+                <?php
+                // on boucle sur la sous categ:
+                
+                    foreach($getAllSub as $sub){
+                        // on recupère l'ID:
+                        $subId = $sub -> getMwIdSubCategory();
+                        
+                        // On recupère toutes les ressources avec les ID des categ et sous categ en même temps :
+                        $getAllByAll = $ressourceManager -> getAllbyAll($categId, $subId);
 
-        // on verifie getAllByAll est pas vide :
-        if(!empty($getAllByAll)){
-            // on affiche le titre de la sous categ, on l'a mis dans le if comme ça le titre de la sous categ ne s'affiche que s'il y a un article dedans :
-            echo "<h3> sous category : " . $sub-> getMwTitleSubCategory() . "</h3><br><br>";
-            // on boucle sur les ressources :
-       ?>
-          </div>
-         <div>
-          <?php
-            
-            foreach($getAllByAll as $all){
-                if(!empty($all)){
-                    // on affiche les ressources:
-                    echo "<p><strong>contenu : " . $all -> getMwTitleRessource() . "</strong><p><br>"; 
+                        // on verifie getAllByAll est pas vide :
+                        if(!empty($getAllByAll)){
+                            // on affiche le titre de la sous categ, on l'a mis dans le if comme ça le titre de la sous categ ne s'affiche que s'il y a un article dedans :
+                            echo "<h3> sous category : " . $sub-> getMwTitleSubCategory() . "</h3><br><br>";
+                            // on boucle sur les ressources :
+                ?>
+            </div>
+                <div>
+                    <?php
+                        foreach($getAllByAll as $all){
+                            if(!empty($all)){
+                                // on affiche les ressources:
+                                echo "<p><strong>contenu : " . $all -> getMwTitleRessource() . "</strong><p>"; 
                     ?>
-                    <div class="grid_ressources">
-                        <?php
-                    echo "<p class=''>" . $all -> getMwContentRessource() . "<p><br>"; 
-                    if (!empty($all->getMwPictureMwIdPicture())){
-                        ?>
-                        <img src="<?= $pictureManager -> getOneById($all -> getMwPictureMwIdPicture()) ->getMwUrlPicture() ?>" alt=""> <br>
-                        <?php
-                      echo "<p>" . $pictureManager -> getOneById($all -> getMwPictureMwIdPicture()) ->getMwUrlPicture() . "<p><br>"; 
-                    }
-                    echo "<p>" . $all -> getMwUrlRessource() . "<p><br>"; 
-                      
+                            <div class="grid_ressources">
+                            <?php
+                                echo "<p  class='img_ressources'>" . $all -> getMwContentRessource() . "<p>"; 
+                                if (!empty($all->getMwPictureMwIdPicture())){
+                            ?>
+                               <!-- <img src="<?= $pictureManager -> getOneById($all -> getMwPictureMwIdPicture()) ->getMwUrlPicture() ?>" alt=""> -->
+                                    <?php
+                                    echo "<p>" . $pictureManager -> getOneById($all -> getMwPictureMwIdPicture()) ->getMwUrlPicture() . "<p>"; 
+                                    }
+                                    echo "<a href=>" . $all -> getMwUrlRessource() . "<a><div class='empty'></div>"; 
+                                    
+                            }
+                        }
                 }
             }
         }
-    }
-}
-?>
-</div>
-</div>
-</div>
+        ?>
+                            </div>
+                </div>
+    </div>
+
 </main>
 <?php
 // FOOTER
